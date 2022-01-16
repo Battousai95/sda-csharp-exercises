bfa0f955c5250ef206189f8a78dcff794d6310d3
User <dawidos123-95@o2.pl>
2022-01-16 14:29:05 +0100

    Rozwiązanie zadania E02

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,9 +4,26 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+
+        static void Potegowanie(int podstawa, int wykladnik)
+        {
+            int wynik = podstawa;
+            for(int i=1;i<wykladnik;i++)
+            {
+                wynik *= podstawa;
+            }
+            Console.Write($"Wynik: {wynik}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Podaj podstawę i wykładnik: ");
+            string s = Console.ReadLine();
+            string[] a = s.ToString().Split(' ');
+
+            if (int.Parse(a[1]) > 0)
+                Potegowanie(int.Parse(a[0]), int.Parse(a[1]));
+            else
+                Console.WriteLine("Zły wykładnik");
         }
     }
 }
