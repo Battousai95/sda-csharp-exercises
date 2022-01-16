0dfca68ce201eacff56d147884390aefa1d4328a
User <dawidos123-95@o2.pl>
2022-01-16 14:05:00 +0100

    Wykonanie zadania E01

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,9 +4,18 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static void Wyswietl(int mnoznik)
+        {
+            Console.WriteLine($"Liczba sztuk: {mnoznik} łączna cena: {mnoznik * 5}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ile linii: ");
+            int linie = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= linie; i++)
+                Wyswietl(i);
         }
     }
 }
